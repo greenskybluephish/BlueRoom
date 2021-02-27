faba4a2a33fa2eaaf7038ad51f3524d45f2bc254
greenskybluephish <briantjobe@gmail.com>
2021-02-27 17:35:42 -0600

    cmt

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <Header />
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200 h-screen">
+    <Footer class=" bottom-4" />
+    <div class="h-16" />
     <router-view />
-    <Footer />
+   
+    
   </main>
 </template>
